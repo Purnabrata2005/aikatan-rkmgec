9234df3f5d26923338fe4710f074e5a37b30412a
PURNABRATA DEY <deypurnabrata2005@gmail.com>
2026-03-29 01:36:39 +0530

    feat: Add FAQ, gallery, sponsor, team data, and timeline data modules
    - Created FAQ data structure with various questions and answers.
    - Implemented a gallery image array for displaying event images.
    - Added sponsor data structure to manage sponsor logos.
    - Introduced team member data structure with social media links.
    - Developed a timeline data structure for event scheduling.
    - Added utility functions for class name management.
    - Created a galaxy background CSS for enhanced visual effects.
    - Configured Tailwind CSS with custom themes and animations.
    - Initialized TypeScript configuration for the project.

---
+++ src/data/event-data.ts
@@ -0,0 +1,329 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export type EventDataType = {
+  id: string;
+  title: string;
+  image: string;
+  buttonText: string;
+  prizePool: string;
+  prize: number;
+  time: string;
+  location: string;
+  contact: string;
+  lastDate: string;
+  registrationLink: string;
+  type: 'techlavya' | 'esport';
+}
+
+
+export const TechlavyaEventData: EventDataType[] = [
+  {
+    id: uuidv4(),
+    title: 'HACKATHON',
+    image: '/events/tech-event/Hackvengers_Web_Picsay_Riyan.png',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 1000,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/ajzqMfXUXeRrPcBG9',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'CODETHON',
+    image: '/events/tech-event/0-Gravity_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/WxxtrhwMbtq2PpzP9',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'AUTOCAD',
+    image: '/events/tech-event/Cad_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/SYxCZjnKkNrSTpNc8',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'ROBORUSH',
+    image: '/events/tech-event/Roborush_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://unstop.com/o/A9EHUry?lb=cDOdwZXp&utm_medium=Share&utm_source=shortUrl',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'PROJECT EXHIBITION',
+    image: '/events/tech-event/Acadexpo_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://unstop.com/o/WULgwDh?lb=cDOdwZXp&utm_medium=Share&utm_source=shortUrl',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'CIRCUIT DESIGN',
+    image: '/events/tech-event/Circuit_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://unstop.com/o/XjsU4T5?lb=cDOdwZXp&utm_medium=Share&utm_source=shortUrl',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'TECH QUIZ',
+    image: '/events/tech-event/Brainbuster_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/oTfk15d36X9mZLhu6',
+    type: 'techlavya'
+  },
+  {
+    id: uuidv4(),
+    title: 'TYPING TEST',
+    image: '/events/tech-event/Typing_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://unstop.com/o/QhmyDCc?lb=cDOdwZXp&utm_medium=Share&utm_source=shortUrl',
+    type: 'techlavya'
+  }
+]
+
+export const EsportsEventData: EventDataType[] = [
+  {
+    id: uuidv4(),
+    title: 'FREE FIRE MAX',
+    image: '/events/esports-event/Freefire_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/AG4SdvVqsBr8GuNH6',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'BGMI',
+    image: '/events/esports-event/Bgmi_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/PqyW4Ty3pKFWN8fdA',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'eFootball',
+    image: '/events/esports-event/Efootball_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/yMSyy6n3khNb59Gc8',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'Mini Militia',
+    image: '/events/esports-event/Minimilitia_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/2tmTyfAV7dCeVvMA9',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'Valorant (PC)',
+    image: '/events/esports-event/Valorant_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/39ZuYHdLjWgg5mGx9',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'FIFA (PC)',
+    image: '/events/esports-event/Fifa_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/sxVgXs6AoMLG2a5r9',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'Asphalt 9',
+    image: '/events/esports-event/Asphalt_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/z2xAWzFZCcMKWsBF7',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'Clash of Clans',
+    image: '/events/esports-event/Clash_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/RnixY7wswCxBRDLg6',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: 'Chess',
+    image: '/events/esports-event/Chess_Web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/VkTbWWGmH7xHeUYA7',
+    type: 'esport'
+  },
+  {
+    id: uuidv4(),
+    title: "Rubik's Cube",
+    image: '/events/esports-event/Rubik_web_Picsay_Riyan.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    prize: 0,
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: 'https://forms.gle/FZ9cPDVkXM6kutV39',
+    type: 'esport'
+  },
+]
+
+
+type CulturalEventDataType = {
+  id: string;
+  title: string;
+  image: string;
+  buttonText: string;
+  prizePool: string;
+  time: string;
+  location: string;
+  contact: string;
+  lastDate: string;
+  registrationLink: string;
+}
+
+
+export const CulturalEventData: CulturalEventDataType[] = [
+  {
+    id: uuidv4(),
+    title: 'Rocking Twilight',
+    image: '/events/cultural-event/twilight.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: ''
+  },
+  {
+    id: uuidv4(),
+    title: 'Velvet Souls',
+    image: '/events/cultural-event/octaves.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: ''
+  },
+  {
+    id: uuidv4(),
+    title: 'Highway',
+    image: '/events/cultural-event/highway.webp',
+    buttonText: 'Tap in Register',
+    prizePool: 'Yet to be announced',
+    time: 'Yet to be announced',
+    location: 'Yet to be announced',
+    contact: 'Yet to be announced',
+    lastDate: 'Yet to be announced',
+    registrationLink: ''
+  }
+]
---
+++ src/data/team-data.ts
@@ -0,0 +1,138 @@
+export interface TeamMember {
+  name: string;
+  role: string;
+  image: string;
+  instagram?: string;
+  twitter?: string;
+  facebook?: string;
+  linkedin?: string;
+}
+
+export const teamMembers: TeamMember[] = [
+  {
+    "name": "Sayan Chakraborty",
+    "role": "President",
+    "image": "/team/1.webp",
+    "facebook": "https://www.facebook.com/profile.php?id=100053096809515&mibextid=ZbWKwL",
+    "instagram": "https://www.instagram.com/stands_to_infinity?igsh=eTd0dnhqMjA0ejht",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/sayan-chakraborty-783167226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Soumen Santra",
+    "role": "Vice-President",
+    "image": "/team/2.webp",
+    "facebook": "https://www.facebook.com/share/1AEH913Uv2/",
+    "instagram": "",
+    "twitter": "https://x.com/Soumen049?t=spFOXk4zEPFaQaDYwTaiuA&s=09",
+    "linkedin": "https://www.linkedin.com/in/soumensantra"
+  },
+  {
+    "name": "Rabin Mahato",
+    "role": "Convenor",
+    "image": "/team/3.webp",
+    "facebook": "",
+    "instagram": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "name": "Akash Sharma",
+    "role": "Vice-Convenor",
+    "image": "/team/4.webp",
+    "facebook": "https://www.facebook.com/share/1EViXiPAoo/",
+    "instagram": "https://www.instagram.com/iamakash_1910?igsh=MWFlN2k3Y3Nna3J0dQ==",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "name": "Annesha Dutta",
+    "role": "Secretary",
+    "image": "/team/5.webp",
+    "facebook": "https://www.facebook.com/share/166ZzZTyG2/",
+    "instagram": "https://www.instagram.com/annesha30_?igsh=NDg4b21hbWJzcjFy",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "name": "Supra Mandal",
+    "role": "Vice-Secretary",
+    "image": "/team/6.webp",
+    "facebook": "https://www.facebook.com/share/15rDpiXap5/",
+    "instagram": "",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/supra-mandal-219145295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Aniket Shaw",
+    "role": "Advisor",
+    "image": "/team/7.webp",
+    "facebook": "",
+    "instagram": "",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/aniketshaw00x/"
+  },
+  {
+    "name": "Suresh Pal",
+    "role": "Web Designer Head",
+    "image": "/team/8.webp",
+    "facebook": "https://www.facebook.com/people/Suresh-Pal/pfbid02JY5bhEzEtK5CDn5b4ANRu2SmHZdPyc3BMVy6dVePsLkkeuhyyd6Mw3micG2qH9DRl/?mibextid=2JQ9oc",
+    "instagram": "https://www.instagram.com/sureshpal8158/",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/suresh61/"
+  },
+  {
+    "name": "Ankur Ghoshal",
+    "role": "Budget Head",
+    "image": "/team/9.webp",
+    "facebook": "https://www.facebook.com/profile.php?id=100011371799852",
+    "instagram": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "name": "Dipmoy Barik",
+    "role": "Budget Co-Head",
+    "image": "/team/10.webp",
+    "facebook": "https://www.facebook.com/share/1L4BbPDkTy/",
+    "instagram": "https://www.instagram.com/dipmoy369?igsh=MXM1Z3Uxcjd5MGZocw==",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "name": "Manish Hira",
+    "role": "Sponsorship Head",
+    "image": "/team/11.webp",
+    "facebook": "https://www.facebook.com/manish.hira.399?mibextid=9V8EeZkStBqWdiND",
+    "instagram": "https://www.instagram.com/manish_hira01?igsh=dXF0b2htaXlmdHdj",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/manish-hira?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Sayan De Karmakar ",
+    "role": "Sponsorship Co-Head",
+    "image": "/team/12.webp",
+    "facebook": "https://www.facebook.com/sayan.dekarmakar.94/about_overview",
+    "instagram": "https://www.instagram.com/sayan2733k/",
+    "twitter": "https://x.com/SayanDK000",
+    "linkedin": "https://www.linkedin.com/in/sayan-de-karmakar-48b296344/"
+  },
+  {
+    "name": "Subhadip Mandal",
+    "role": "Social Media Head",
+    "image": "/team/13.webp",
+    "facebook": "https://www.facebook.com/share/18hCMdiDyb/",
+    "instagram": "https://www.instagram.com/this.is_subhadip/profilecard/?igsh=bHFrdmcxZnN3djJh",
+    "twitter": "",
+    "linkedin": "https://www.linkedin.com/in/subhadip-mandal-052aa4247?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Sourav Sahoo",
+    "role": "Decoration Head",
+    "image": "/team/14.webp",
+    "facebook": "",
+    "instagram": "",
+    "twitter": "",
+    "linkedin": ""
+  }
+];
